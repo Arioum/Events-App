23019bafcf5a366fbf95993ba68b9d5256d02b51
arioum <amoghrao121@gmail.com>
2022-11-22 23:44:30 +0530

    updated slug and events page

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,8 +1,23 @@
+import { useRouter } from "next/router";
+import { getAllEvents } from "../../dummy-data"
+import EventList from '../../components/events/EventList'
+import EventsSearch from "../../components/events/EventsSearch";
 
-const EventsPage = () => {
+const AllEventsPage = () => {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`
+    router.push(fullPath)
+  }
+
   return (
-    <div>All Events</div>
+    <>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </>
   )
 }
 
-export default EventsPage
+export default AllEventsPage
